bf8215b72dce8cf758cc9e215fd2dabf39215a75
Ortwin Van der Stappen <ortwinvds@gmail.com>
2024-07-27 22:36:14 +0100

    portfolio update

--- index.js
+++ index.js
@@ -13,45 +13,45 @@ function insertEmail() {
 }
 
 /* -----------------------------------------
-  Have focus outline only for keyboard users 
+  Have focus outline only for keyboard users
  ---------------------------------------- */
 
-const handleFirstTab = (e) => {
-  if (e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+// const handleFirstTab = (e) => {
+//   if (e.key === 'Tab') {
+//     document.body.classList.add('user-is-tabbing')
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
-  }
+//     window.removeEventListener('keydown', handleFirstTab)
+//     window.addEventListener('mousedown', handleMouseDownOnce)
+//   }
 
-}
+// }
 
-const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+// const handleMouseDownOnce = () => {
+//   document.body.classList.remove('user-is-tabbing')
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
+//   window.removeEventListener('mousedown', handleMouseDownOnce)
+//   window.addEventListener('keydown', handleFirstTab)
+// }
 
-window.addEventListener('keydown', handleFirstTab)
+// window.addEventListener('keydown', handleFirstTab)
 
-const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
+// const backToTopButton = document.querySelector(".back-to-top");
+// let isBackToTopRendered = false;
 
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
-};
+// let alterStyles = (isBackToTopRendered) => {
+//   backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+//   backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+//   backToTopButton.style.transform = isBackToTopRendered
+//     ? "scale(1)"
+//     : "scale(0)";
+// };
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 700) {
+//     isBackToTopRendered = true;
+//     alterStyles(isBackToTopRendered);
+//   } else {
+//     isBackToTopRendered = false;
+//     alterStyles(isBackToTopRendered);
+//   }
+// });
